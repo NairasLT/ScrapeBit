f6b5e693c0d95d0a6b24a48c23c0cbd45b4b5323
NairasLT <35661992+NairasLT@users.noreply.github.com>
2020-12-06 13:36:28 +0200

    Add Array Support For Javascript Version

--- ScrapeBit.js
+++ ScrapeBit.js
@@ -1,10 +1,5 @@
-//  Example
-// string Input = "<h1>dasds{3123}sssdf,value:awesomevalue]f//wessa
-// const Result = ScapeBitString(Input, "value:", "]f/", 40);
-// Result = awesomevalue
-
-module.exports = ScrapeBitString;
-function ScrapeBitString(SourceText, ToFindText, ReadUntilString) {
+module.exports = { String, AllString };
+function String(SourceText, ToFindText, ReadUntilString) {
 
     const dIndex = SourceText.indexOf(ToFindText); // Get Index in string of the find Text.
 
@@ -33,4 +28,64 @@ function ScrapeBitString(SourceText, ToFindText, ReadUntilString) {
 
     if (ReadTextChars.length <= 0) return null;
     else return ReadTextChars.join("");
+}
+
+function AllString(Source, Find, ReadUntil) {
+    var Indexes = getIndicesOf(Find, Source, false);
+    var Results = [];
+
+    for (let index of Indexes) {
+        Results.push(FromIndex(Source, Find, ReadUntil, index))
+    }
+    return Results;
+}
+
+function FromIndex(SourceText, ToFindText, ReadUntilString, CustomIndex) {
+
+    const dIndex = CustomIndex; // Get Index in string of the find Text.
+
+    if (dIndex === -1) return null // If Find Text is not found return.
+
+    SourceText = SourceText.substring(dIndex + ToFindText.length);
+
+    let FoundTerminators = 0; // ? 0 index based
+    let ReadTextChars = []; //Where to store the good chars.
+
+    SourceText = [...SourceText]; // To Char Array
+    const TerminatorChars = [...ReadUntilString]; //To Char Array
+
+    for (let i = 0; i < SourceText.length; i++) {
+
+        if (FoundTerminators === TerminatorChars.length) break;
+
+        if (SourceText[i] === TerminatorChars[FoundTerminators]) {
+            FoundTerminators++;
+            continue;
+        }
+
+        FoundTerminators = 0;
+        ReadTextChars.push(SourceText[i]); // Add the good char
+    }
+
+    if (ReadTextChars.length <= 0) return null;
+    else return ReadTextChars.join("");
+}
+
+function getIndicesOf(searchStr, str, caseSensitive) {
+    var searchStrLen = searchStr.length;
+    if (searchStrLen == 0) {
+        return [];
+    }
+
+    var startIndex = 0, index, indices = [];
+    if (!caseSensitive) {
+        str = str.toLowerCase();
+        searchStr = searchStr.toLowerCase();
+    }
+
+    while ((index = str.indexOf(searchStr, startIndex)) > -1) {
+        indices.push(index);
+        startIndex = index + searchStrLen;
+    }
+    return indices;
 }
